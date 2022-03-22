46eb80f327d5e20348bd4dec2d8f51ac0854a996
Genius1512 <silvan.genius@gmail.com>
2022-03-22 19:09:03 +0100

    Fixed build and removed bin

--- .pyinit/build.sh
+++ .pyinit/build.sh
@@ -5,15 +5,13 @@ echo ""
 
 time="$(date +%s)"
 
-pyinstaller --noconfirm --onefile --icon "$(cd $(dirname $(dirname "${BASH_SOURCE[0]}")) && pwd)/icon.ico" --name $name "$(cd $(dirname $(dirname "${BASH_SOURCE[0]}")) && pwd)/scripts/__main__.py"
+pyinstaller --noconfirm --icon "$(cd $(dirname $(dirname "${BASH_SOURCE[0]}")) && pwd)/icon.ico" --name $name "$(cd $(dirname $(dirname "${BASH_SOURCE[0]}")) && pwd)/scripts/__main__.py"
 
 time="$(($(date +%s)-time))"
 
 echo ""
 echo "<----------BUILD---------->"
 echo "Done in $time seconds"
-
-pyinstaller --noconfirm --onefile --icon "icon.ico" --name $name "scripts/__main__.py"
 
 mv "dist/$name" "$(cd $(dirname $(dirname "${BASH_SOURCE[0]}")) && pwd)/bin"
 rm -rf "dist"
